8881f37b7e025ab035451c1530ff12f77471e862
hyungwoo-choi <tinbed122@gmail.com>
2024-06-02 16:22:34 +0900

    8주차

---
+++ src/main/java/umc/spring/study/apiPayload/code/ErrorReasonDTO.java
@@ -0,0 +1,18 @@
+package umc.spring.study.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class ErrorReasonDTO {
+    private String message;
+    private String code;
+    private boolean isSuccess;
+    private HttpStatus httpStatus;
+}
---
+++ src/main/java/umc/spring/study/converter/TempConverter.java
@@ -0,0 +1,16 @@
+package umc.spring.study.converter;
+
+import umc.spring.study.web.dto.TempResponse;
+
+public class TempConverter {
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is test")
+                .build();
+    }
+    public static TempResponse.TempExceptionDTO toTempExceptionDTO(Integer flag){
+        return TempResponse.TempExceptionDTO.builder()
+                .flag(flag)
+                .build();
+    }
+}
